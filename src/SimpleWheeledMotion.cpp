f8d68e1a5c39cf85c16b983e74d4a0d3b23eb792
Arturo Laurenzi <arturo.laurenzi@iit.it>
2018-03-20 17:12:58 +0100

    Working version of simple wheeled motion as ci impl

---
+++ src/SimpleWheeledMotion.cpp
@@ -0,0 +1,376 @@
+#include <centauro_tools/SimpleWheeledMotion.h>
+#include <OpenSoT/constraints/velocity/VelocityLimits.h>
+#include <OpenSoT/constraints/velocity/JointLimits.h>
+
+
+extern "C" XBot::Cartesian::CartesianInterface* create_instance(XBot::ModelInterface::Ptr model,
+                                                                XBot::Cartesian::ProblemDescription pb)
+{
+    return new XBot::Cartesian::WheeledMotionImpl(model);
+}
+
+extern "C" void destroy_instance( XBot::Cartesian::CartesianInterface* instance )
+{
+    delete instance;
+}
+
+namespace XBot { namespace Cartesian {
+
+WheeledMotionImpl::WheeledMotionImpl(ModelInterface::Ptr model):
+    CartesianInterfaceImpl(model, __generate_tasks()),
+    _logger(MatLogger::getLogger("/tmp/wheeled_motion_log"))
+{
+    _model->getJointPosition(_q);
+    _qpostural = _q;
+    _ddq = _dq = _q*0;
+
+    
+    std::vector<std::string> steering_joints = {"ankle_yaw_1", "ankle_yaw_2", "ankle_yaw_3", "ankle_yaw_4"};
+    std::vector<bool> disable_steering(_model->getJointNum(), true);
+    for(auto j : steering_joints)
+    {
+        disable_steering[ _model->getDofIndex(j) ] = false;
+
+    }
+
+    auto pos_idx = OpenSoT::Indices::range(0,2);
+    auto pos_idx_xy = OpenSoT::Indices::range(0,1);
+    auto pos_idx_z = OpenSoT::Indices::range(2,2);
+    auto or_idx = OpenSoT::Indices::range(3,5);
+    auto or_xy_idx = OpenSoT::Indices::range(3,4);
+
+
+
+    for(int i = 0; i < NUM_WHEELS; i++)
+    {
+        std::string wheel_name = _model->leg(i).getTipLinkName();
+        
+        _steering.emplace_back(_model, wheel_name);
+
+        auto wheel_cartesian_rel = boost::make_shared<CustomRelativeCartesian>(*_model,
+                                                                 wheel_name,
+                                                                 "pelvis"
+                                                                );
+        wheel_cartesian_rel->setActiveJointsMask(disable_steering);
+
+        wheel_cartesian_rel->setLambda(0.01);
+        
+        auto wheel_pos_rel_xy = wheel_cartesian_rel % pos_idx_xy;
+        
+        
+        
+        
+        auto wheel_cartesian = boost::make_shared<CartesianTask>("WHEEL_CART_" + std::to_string(i),
+                                                                 _q,
+                                                                 *_model,
+                                                                 wheel_name,
+                                                                 "world"
+                                                                );
+        
+        wheel_cartesian->setActiveJointsMask(disable_steering);
+
+        wheel_cartesian->setLambda(0.1);
+
+        auto wheel_pos_z = wheel_cartesian % pos_idx_z;
+
+        auto wheel_rolling =  boost::make_shared<RollingTask>(_model->leg(i).getTipLinkName(), RADIUS, *_model);
+
+        std::string pp_link = get_parent(get_parent(wheel_name));
+
+        auto pp_cartesian =   boost::make_shared<CartesianTask>("PP_CART_" + std::to_string(i),
+                                                                 _q,
+                                                                 *_model,
+                                                                 pp_link,
+                                                                 "world"
+                                                                );
+        auto pp_or = pp_cartesian % or_xy_idx;
+
+        _wheel_cart_rel.push_back(wheel_cartesian_rel);
+        _wheel_cart.push_back(wheel_cartesian);
+        _wheel_pos_xy.push_back(wheel_pos_rel_xy);
+        _wheel_pos_z.push_back(wheel_pos_z);
+        _rolling.push_back(wheel_rolling);
+        _pp_cart.push_back(pp_cartesian);
+        _pp_or.push_back(pp_or);
+//         _cartesian_tasks.push_back(wheel_cartesian);
+
+    }
+
+    auto pp_or_xy_aggr = _pp_or[0] + _pp_or[1] + _pp_or[2] + _pp_or[3];
+    auto wheel_pos_aggr = _wheel_pos_xy[0] + _wheel_pos_xy[1] + _wheel_pos_xy[2] + _wheel_pos_xy[3];
+    auto rolling_aggr = _rolling[0] + _rolling[1] + _rolling[2] + _rolling[3];
+    auto wheel_z_aggr = _wheel_pos_z[0] + _wheel_pos_z[1] + _wheel_pos_z[2] + _wheel_pos_z[3];
+
+    _waist_cart = boost::make_shared<CartesianTask>("WAIST_CART",
+                                                    _q,
+                                                    *_model,
+                                                    "pelvis",
+                                                    "world"
+                                                );
+     _waist_cart->setLambda(0.05);
+     _cartesian_tasks.push_back(_waist_cart);
+
+    _postural = boost::make_shared<OpenSoT::tasks::velocity::Postural>(_q);
+    _postural->setLambda(0.05);
+
+
+
+
+    Eigen::VectorXd qdotmax, qmin, qmax;
+    _model->getVelocityLimits(qdotmax);
+    _model->getJointLimits(qmin, qmax);
+    auto velocity_lims = boost::make_shared<OpenSoT::constraints::velocity::VelocityLimits>(qdotmax, 0.01);
+    auto joint_lims = boost::make_shared<OpenSoT::constraints::velocity::JointLimits>(_q, qmax, qmin);
+
+    _autostack = ( 
+                    ( pp_or_xy_aggr + _waist_cart + wheel_z_aggr ) / 
+                    ( rolling_aggr + wheel_pos_aggr + 0.0001 * _postural ) 
+                 ) << velocity_lims << joint_lims;
+                 
+    _autostack->update(_q);
+
+
+    /* Create solver */
+    _solver = boost::make_shared<OpenSoT::solvers::iHQP>(_autostack->getStack(),
+                                                         _autostack->getBounds(),
+                                                         1e-4,
+                                                         OpenSoT::solvers::solver_back_ends::OSQP
+                                                        );
+}
+
+double SimpleSteering::sign(double x)
+{
+    return (x > 0) - (x < 0);
+}
+
+
+bool WheeledMotionImpl::update(double time, double period)
+{
+    bool success = true;
+
+    XBot::Cartesian::CartesianInterfaceImpl::update(time, period);
+
+    _model->getJointPosition(_q);
+
+    /* Update reference for all cartesian tasks */
+    for(auto cart_task : _cartesian_tasks)
+    {
+        Eigen::Affine3d T_ref;
+        Eigen::Vector6d v_ref, a_ref;
+
+        if(getControlMode(cart_task->getDistalLink()) == ControlType::Disabled)
+        {
+            cart_task->setActive(false);
+            continue;
+        }
+
+        if(getControlMode(cart_task->getDistalLink()) == ControlType::Velocity)
+        {
+
+            cart_task->setLambda(0.0);
+        }
+
+        if(!getPoseReference(cart_task->getDistalLink(), T_ref, &v_ref, &a_ref))
+        {
+            continue;
+        }
+
+        cart_task->setActive(true);
+        cart_task->setReference(T_ref.matrix(), v_ref);
+
+    }
+    
+    for(auto cart_task : _wheel_cart_rel)
+    {
+        Eigen::Affine3d T_ref;
+        Eigen::Vector6d v_ref, a_ref;
+
+        if(getControlMode(cart_task->getDistalLink()) == ControlType::Disabled)
+        {
+            cart_task->setActive(false);
+            continue;
+        }
+
+        if(getControlMode(cart_task->getDistalLink()) == ControlType::Velocity)
+        {
+
+            cart_task->setLambda(0.0);
+        }
+
+        if(!getPoseReference(cart_task->getDistalLink(), T_ref, &v_ref, &a_ref))
+        {
+            continue;
+        }
+
+        cart_task->setActive(true);
+        cart_task->setReference(T_ref.translation());
+    }
+    
+    
+
+    
+    _autostack->update(_q);
+    _autostack->log(_logger);
+
+    if(!_solver->solve(_dq))
+    {
+        _dq.setZero(_dq.size());
+        XBot::Logger::error("OpenSot: unable to solve\n");
+        success = false;
+    }
+    
+    /* Steering */
+    Eigen::Vector3d waist_vref = _waist_cart->getError().head<3>();
+    Eigen::Vector3d wheel_vref(0, 0, 0);
+    _logger->add("waist_vref", waist_vref);
+    for(int i = 0; i < NUM_WHEELS; i++)
+    {
+        auto& steering = _steering[i];
+        
+        wheel_vref = _wheel_cart_rel[i]->getError().head<3>();
+        _qpostural(steering.getDofIndex()) = steering.computeSteeringAngle(waist_vref, wheel_vref);
+        _dq(steering.getDofIndex()) = 0.1*(_qpostural(steering.getDofIndex()) - _q(steering.getDofIndex()));
+        
+        _logger->add("wheel_vref_"+std::to_string(i+1), wheel_vref);
+        _logger->add("steering_angle_"+std::to_string(i+1), _qpostural(steering.getDofIndex()));
+        
+        std::cout << _qpostural(steering.getDofIndex()) << std::endl;
+    }
+
+    _dq /= period;
+    _model->setJointVelocity(_dq);
+    _model->setJointAcceleration(_ddq);
+
+    _logger->add("q", _q);
+
+
+    return success;
+
+
+}
+
+std::string WheeledMotionImpl::get_parent(std::string link)
+{
+    return _model->getUrdf().getLink(link)->parent_joint->parent_link_name;
+}
+
+std::vector< std::pair< std::string, std::string > > WheeledMotionImpl::__generate_tasks()
+{
+    std::vector< std::pair< std::string, std::string > > tasks;
+    tasks.emplace_back("world", "pelvis");
+    tasks.emplace_back("world", "wheel_1");
+    tasks.emplace_back("world", "wheel_2");
+    tasks.emplace_back("world", "wheel_3");
+    tasks.emplace_back("world", "wheel_4");
+
+    return tasks;
+}
+
+WheeledMotionImpl::~WheeledMotionImpl()
+{
+    _logger->flush();
+}
+
+SimpleSteering::SimpleSteering(XBot::ModelInterface::ConstPtr model, 
+                               std::string wheel_name):
+    _model(model),
+    _wheel_name(wheel_name)
+{
+    auto spinning_axis = _model->getUrdf().getLink(wheel_name)->parent_joint->axis;
+    _wheel_spinning_axis << spinning_axis.x, spinning_axis.y, spinning_axis.z;
+    
+    std::string wheel_parent_name = _model->getUrdf().getLink(wheel_name)->parent_joint->parent_link_name;
+    auto steering_axis = _model->getUrdf().getLink(wheel_parent_name)->parent_joint->axis;
+    _world_steering_axis << steering_axis.x, steering_axis.y, steering_axis.z;
+    Eigen::Matrix3d w_R_wp;
+    _model->getOrientation(wheel_parent_name, w_R_wp);
+    _world_steering_axis = w_R_wp * _world_steering_axis;
+    
+    
+    _steering_id = _model->getDofIndex(_model->getUrdf().getLink(wheel_parent_name)->parent_joint->name);
+    _steering_joint = _model->getJointByName(_model->getUrdf().getLink(wheel_parent_name)->parent_joint->name);
+    
+    _model->getFloatingBaseLink(_waist_name);
+}
+
+double SimpleSteering::wrap_angle(double x)
+{
+    x = fmod(x + M_PI, 2 * M_PI);
+    if (x < 0)
+        x += 2 * M_PI;
+    return x - M_PI;
+}
+
+double SimpleSteering::getDofIndex() const
+{
+    return _steering_id;
+}
+
+
+double SimpleSteering::computeSteeringAngle(const Eigen::Vector3d& waist_vel, 
+                                            const Eigen::Vector3d& __wheel_vel)
+{
+    Eigen::Vector3d wheel_vel = __wheel_vel;
+    
+    
+    /* Wheel orientation */
+    Eigen::Affine3d w_T_wheel;
+    _model->getPose(_wheel_name, w_T_wheel);
+    
+    Eigen::Affine3d w_T_waist;
+    _model->getPose(_waist_name, w_T_waist);
+    
+    /* Steering joint angle */
+    _model->getJointPosition(_q);
+    double q = _q(_steering_id);
+    
+    /* Current angle */
+    Eigen::Vector3d wheel_forward = (w_T_wheel.linear()*_wheel_spinning_axis).cross(Eigen::Vector3d::UnitZ());
+    wheel_forward.normalize();
+    double theta = std::atan2(wheel_forward.y(), wheel_forward.x());
+    
+    /* Desired angle */
+    Eigen::Vector3d r = w_T_wheel.translation() - w_T_waist.translation();
+    Eigen::Vector3d vdes = wheel_vel + waist_vel + waist_vel.z()*Eigen::Vector3d::UnitZ().cross(r);
+    
+    if( vdes.head(2).norm() < 0.001 ) // TBD compare with histeresys
+    {
+        vdes << 1.0, 0.0, 0.0;
+    }
+    
+    std::cout << "ves: " << vdes.transpose() << std::endl;
+    double des_theta_1 = std::atan2(vdes.y(), vdes.x());
+    
+    double des_q_1 = q  + (des_theta_1 - theta)*_world_steering_axis.z();
+    des_q_1 = wrap_angle(des_q_1);
+    
+    double des_q_2 = des_q_1 + M_PI;
+    des_q_2 = wrap_angle(des_q_2);
+    
+    double closest_q = std::fabs(des_q_1-q) < std::fabs(des_q_2-q) ? des_q_1 : des_q_2;
+    
+    /* Return value inside joint lims */
+    if(_steering_joint->checkJointLimits(closest_q))
+    {
+        return closest_q;
+    }
+    else if(_steering_joint->checkJointLimits(des_q_1))
+    {
+        return des_q_1;
+    }
+    else if(_steering_joint->checkJointLimits(des_q_2))
+    {
+        return des_q_2;
+    }
+    else{
+        throw std::runtime_error("Unable to find steering angle");
+    }
+
+    
+}
+
+
+
+
+
+} }
