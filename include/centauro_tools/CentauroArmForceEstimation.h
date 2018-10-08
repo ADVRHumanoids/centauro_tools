83bbfff8878f4181cfc44aa0601cd05ebcfcea03
Arturo Laurenzi <arturo.laurenzi@iit.it>
2018-10-08 12:20:22 +0200

    Terrible code for force estimation on the right arm

---
+++ include/centauro_tools/CentauroArmForceEstimation.h
@@ -0,0 +1,54 @@
+#ifndef __MPL_CENTAURO_FORCE_EST_H__
+#define __MPL_CENTAURO_FORCE_EST_H__
+
+#include <XBotInterface/RobotInterface.h>
+#include <vector>
+
+namespace centauro {
+
+    
+    class ForceEstimation 
+    {
+        
+    public:
+        
+        typedef std::shared_ptr<ForceEstimation> Ptr;
+        
+        static const int N_J = 4;
+        
+        /**
+         * @brief Contruct the force estimation class.
+         * 
+         * @param model Model that is kept updated with the robot state
+         */
+        ForceEstimation(const XBot::ModelInterface& model);
+        
+        /**
+         * @brief Call this function to compute an estimate based on the current model state.
+         */
+        void compute();
+        
+        Eigen::Vector3d getForce(Eigen::VectorXd * tau_res = nullptr) const;
+        
+        ~ForceEstimation();
+        
+    private:
+        
+        const XBot::ModelInterface& _model;
+        XBot::MatLogger::Ptr _logger;
+        
+        std::string _ee_name;
+        Eigen::Vector3d _force;
+        Eigen::VectorXd _res;
+        
+        Eigen::JacobiSVD<Eigen::Matrix<double, N_J, 3>> _svd;
+        
+        Eigen::VectorXd _tau, _nl;
+        Eigen::MatrixXd _J;
+        
+    };
+    
+}
+
+
+#endif
